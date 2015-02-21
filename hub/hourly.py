f414a9815f94e0d03a76a929bfe684da86b0f1a1
Harold Cooper <hrldcpr@gmail.com>
2015-02-20 19:32:07 -0500

    quick script for sending AT commands via API

--- hub/hourly.py
+++ hub/hourly.py
@@ -1,15 +1,27 @@
 import logging
+import sys
+import time
 
 import serial
 
+from . import common
 
-def main():
-    with serial.Serial('/dev/ttyAMA0') as xbee:
+
+def main(device='/dev/ttyAMA0'):
+    with serial.Serial(device) as xbee:
         logging.info('connected to xbee.')
-        frame = b'\x7E\x00\x04\x08\x00SP\x05\x7C\x4C'
-        xbee.write(frame)
-        xbee.write(common.checksum(frame))
+
+        cmd = b'\x08zAG\xFF\xFF'
+        msg = b'\x7E' + bytes([0, len(cmd)]) + cmd + bytes([0xFF - common.checksum(cmd)])
+
+        print('sending {}'.format(repr(msg)))
+        xbee.write(msg)
+        print('sent')
+        
+        while True:
+            print(repr(xbee.read(1)))
+            time.sleep(0.1)
 
 if __name__ == '__main__':
     logging.info('starting...')
-    main()
+    main(sys.argv[1])
