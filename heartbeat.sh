523f59258fdca11245208d55d875ae130da01dc7
Harold Cooper <harold@heatseeknyc.com>
2015-02-24 21:46:49 -0500

    heartbeat.sh POSTs every 10 minutes, attempts to fix wvdial and modem if there's no internet, and after 6 hours gives up and reboots. also switched ssh supervisor log back to AUTO so that it is deleted on boot, since it no longer indicates a valid port

--- heartbeat.sh
+++ heartbeat.sh
@@ -4,27 +4,36 @@ PI_ID=$(awk '/^Serial\t/ { print $3 }' /proc/cpuinfo)
 # TODO vnstat -i ppp0 --oneline
 
 PORT=""
+FAILURES=0
 while true
 do
     # check if our tunnel port has changed, once a minute for up to 10 minutes:
     for _ in $(seq 10)
-    do NEWPORT=$(supervisorctl tail ssh stderr | awk '/^Allocated port / { print $3 }' | tail -1)
-	if [ "$NEWPORT" != "$PORT" ]
-	then break
-	fi
+    do NEWPORT=$(supervisorctl tail ssh | awk '/^Allocated port / { print $3 }' | tail -1)
+	[ "$NEWPORT" != "$PORT" ] && break
 	sleep 60
     done
 
     # port has changed or 10 minutes have passed, so time to send a heartbeat:
+    echo "posting hub=$PI_ID&port=$NEWPORT..."
     if curl -sS -d "hub=$PI_ID" -d "port=$NEWPORT" http://relay.heatseeknyc.com/hubs
-    then PORT="$NEWPORT"
+    then echo # server response often has no newline
+	PORT="$NEWPORT"
+	FAILURES=0
     elif ! ping -c 1 google.com
     then # internet is down :'(
-	supervisorctl stop wvdial
-	killall wvdial
-	usb_modeswitch -I -W -D -s 20 -u -1 -v 12d1 -p 1446 -c conf/usb_modeswitch.conf
-	sleep 1
-	supervisorctl start wvdial
+	echo "$((++FAILURES)) failures in a row!"
+	if [ $FAILURES -lt 36 ] # 6*6*10 minutes = 6 hours
+	then # try to fix it
+	    supervisorctl stop wvdial
+	    killall wvdial
+	    usb_modeswitch -I -W -D -s 20 -u -1 -v 12d1 -p 1446 -c conf/usb_modeswitch.conf
+	    sleep 5
+	    supervisorctl start wvdial
+	else # give up and try rebooting...
+	    reboot
+	    exit
+	fi
     fi
 
     sleep 1
