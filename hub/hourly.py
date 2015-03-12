0db9aba6f3b08da346bd35d9bb2b80eda427acd8
Harold Cooper <hrldcpr@gmail.com>
2015-03-12 12:19:41 -0400

    script for going from setup mode -> live mode

--- hub/hourly.py
+++ hub/hourly.py
@@ -6,21 +6,19 @@ import serial
 
 from . import common
 
+def send(command, xbee):
+    frame = b'\x7E' + bytes([0, len(command)]) + command + bytes([0xFF - common.checksum(command)])
+    print('sending {}'.format(repr(frame)))
+    xbee.write(frame)
+    print('sent')
 
 def main(device='/dev/ttyAMA0'):
     with serial.Serial(device) as xbee:
         logging.info('connected to xbee.')
 
-        cmd = b'\x08zAG\xFF\xFF'
-        msg = b'\x7E' + bytes([0, len(cmd)]) + cmd + bytes([0xFF - common.checksum(cmd)])
-
-        print('sending {}'.format(repr(msg)))
-        xbee.write(msg)
-        print('sent')
-        
-        while True:
-            print(repr(xbee.read(1)))
-            time.sleep(0.1)
+        send(b'\x081AG\xFF\xFF', xbee)
+        time.sleep(1)
+        send(b'\x082SP\x05\x7A\x58', xbee)
 
 if __name__ == '__main__':
     logging.info('starting...')
