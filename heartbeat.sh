8cf513aec004dca4e0e63666c450045b0a024d78
Harold Cooper <hrldcpr@gmail.com>
2015-08-26 13:06:45 -0400

    tidy heartbeat script

--- heartbeat.sh
+++ heartbeat.sh
@@ -1,25 +1,20 @@
 
-while [ "$XBEE_ID" == "" ]
-do
-    # note this clears any previous id, in case the xbee was changed:
-    python3 -m hub.reset_xbee_id
-    echo "waiting for xbee id to appear in db..."
+while [ "$xbee_id" == "" ]; do
+    python3 -m hub.reset_xbee_id  # note this clears any previous id, in case the xbee was changed
+    echo 'waiting for xbee id to appear in db...'
     sleep 10
-    XBEE_ID=$(python3 -m hub.get_xbee_id)
+    xbee_id=$(python3 -m hub.get_xbee_id)
 done
-echo "xbee id is $XBEE_ID"
+echo "xbee id is $xbee_id"
 
-PI_ID=$(awk '/^Serial\t/ { print $3 }' /proc/cpuinfo)
+pi_id=$(awk '/^Serial\t/ { print $3 }' /proc/cpuinfo)
+echo "π id is $pi_id"
 
-PORT=""
-while true
-do
-    for _ in $(seq 10)  # watch for new tunnel port every minute for up to 10 minutes
-    do
-        NEWPORT=$(supervisorctl tail ssh | awk '/^Allocated port / { print $3 }' | tail -1)
-	if [ "$NEWPORT" != "$PORT" ]
-	then
-            echo "port changed from $PORT to $NEWPORT"
+while true; do
+    for _ in $(seq 10); do  # watch for new tunnel port every minute for up to 10 minutes
+        port=$(supervisorctl tail ssh | awk '/^Allocated port / { print $3 }' | tail -1)
+	if [ "$port" != "$old_port" ]; then
+            echo "port changed to $port from $old_port"
 	    break
 	fi
 	sleep 60
@@ -28,15 +23,14 @@ do
     # TODO also send $(vnstat -i ppp0 --oneline)
 
     # port has changed or 10 minutes have passed, so time to send a heartbeat:
-    # TODO this should be a PUT or PATCH to /hubs/$XBEE_ID
-    echo "posting hub=$XBEE_ID&pi=$PI_ID&port=$NEWPORT..."
-    if curl -sS -d "hub=$XBEE_ID" -d "pi=$PI_ID" -d "port=$NEWPORT" http://relay.heatseeknyc.com/hubs
-    then
-        echo # server response often has no newline
-	PORT="$NEWPORT"
+    # TODO this should be a PUT or PATCH to /hubs/$xbee_id
+    data="hub=$xbee_id&pi=$pi_id&port=$port"
+    echo "posting $data"
+    if curl -sS -d "$data" http://relay.heatseeknyc.com/hubs; then
+        echo  # server response often has no newline
+	old_port="$port"
     else
-	echo "failed to post."
+	echo 'failed to post. waiting to retry...'
+        sleep 60
     fi
-
-    sleep 60
 done
