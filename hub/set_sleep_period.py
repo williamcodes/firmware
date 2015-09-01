51b58f58e45aa9e228ed748149de6e2a48388bc5
Harold Cooper <hrldcpr@gmail.com>
2015-09-01 18:49:44 -0400

    more general set_sleep_period, so that relay has full responsibility for 'live' versus 'setup' sleep periods

---
+++ hub/set_sleep_period.py
@@ -0,0 +1,31 @@
+import argparse
+import logging
+
+import serial
+
+from . import common, xbee
+
+
+def send(frame, xb):
+    print('sending {}'.format(frame))
+    xb.write(frame)
+    print('sent')
+
+@common.main
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('sleep_period', type=int)
+    args = parser.parse_args()
+
+    with serial.Serial('/dev/ttyAMA0') as xb:
+        logging.info('connected to xbee.')
+
+        # 'aggregate' all cells in default broadcast mode to point to us instead:
+        # TODO do this more than once?
+        send(xbee.frame(b'\x08xAG\xFF\xFF'), xb)
+
+        # set sleep period, and write changes to persistent flash
+        send(xbee.frame(b'\x08xSP' + xbee.int_to_bytes(args.sleep_period, 4)), xb)
+        send(xbee.frame(b'\x08xWR'), xb)
+
+        send(xbee.frame(b'\x08xSP'), xb)  # read new sleep period into db
