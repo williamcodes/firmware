2c2c12475703a122d890b0c6d75b4c3e1254bdfe
Harold Cooper <hrldcpr@gmail.com>
2015-09-02 13:41:50 -0400

    delay between xbee commands

--- hub/set_sleep_period.py
+++ hub/set_sleep_period.py
@@ -1,5 +1,6 @@
 import argparse
 import logging
+import time
 
 import serial
 
@@ -23,9 +24,12 @@ def main():
         # 'aggregate' all cells in default broadcast mode to point to us instead:
         # TODO do this more than once?
         send(xbee.frame(b'\x08xAG\xFF\xFF'), xb)
+        time.sleep(0.1)
 
         # set sleep period, and write changes to persistent flash
         send(xbee.frame(b'\x08xSP' + xbee.int_to_bytes(args.sleep_period, 4)), xb)
+        time.sleep(0.1)
         send(xbee.frame(b'\x08xWR'), xb)
+        time.sleep(0.1)
 
         send(xbee.frame(b'\x08xSP'), xb)  # read new sleep period into db
