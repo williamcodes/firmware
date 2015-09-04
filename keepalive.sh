32163b4bdac0dc318d26ee4e02a0c5c0ce98e7a1
Harold Cooper <hrldcpr@gmail.com>
2015-09-03 23:19:54 -0400

    log killing of wvdial

--- keepalive.sh
+++ keepalive.sh
@@ -3,6 +3,7 @@ while true; do
     if ! ping -c 1 relay.heatseeknyc.com; then
         if ! ping -c 1 google.com; then
             if (( ++failures >= 10 )); then
+                echo "$failures failures, killing wvdial."
                 failures=0  # wait 10 minutes until we try again
                 killall wvdial
             fi
