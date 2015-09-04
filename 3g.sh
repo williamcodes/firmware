46a7c77266c4f959de785cf78064dc51b8d4cb85
Harold Cooper <hrldcpr@gmail.com>
2015-09-03 22:47:07 -0400

    simulate unplugging / replugging modem before each wvdial attempt /via @williamcodes

--- 3g.sh
+++ 3g.sh
@@ -1,7 +1,17 @@
 
-while true
-do
-    usb_modeswitch -I -W -D -s 20 -u -1 -v 12d1 -p 1446 -c conf/usb_modeswitch.conf
+while true; do
+    echo 'unbinding USB…'
+    echo '1-1' > /sys/bus/usb/drivers/usb/unbind
+    sleep 5
+    echo 'binding USB…'
+    echo '1-1' > /sys/bus/usb/drivers/usb/bind
+    sleep 15
+    # command copied from usb_modeswitch startup log in debug mode:
+    usb_modeswitch --no-inquire --verbose --sysmode \
+                   --check-success 20 --configuration -1 \
+                   --default-vendor 12d1 --default-product 1446 \
+                   -c conf/usb_modeswitch.conf
+    sleep 10
+
     wvdial
-    sleep 1
 done
