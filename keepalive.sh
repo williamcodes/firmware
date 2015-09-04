c97998d2b3bb4a3c8dcfbc0f6265b65d0ef17b9d
Harold Cooper <hrldcpr@gmail.com>
2015-09-03 22:48:35 -0400

    keepalive script restarts wvdial every 10 minutes that internet is down

---
+++ keepalive.sh
@@ -0,0 +1,14 @@
+
+while true; do
+    if ! ping -c 1 relay.heatseeknyc.com; then
+        if ! ping -c 1 google.com; then
+            if (( ++failures >= 10 )); then
+                failures=0  # wait 10 minutes until we try again
+                killall wvdial
+            fi
+        fi
+    else
+        failures=0  # all is well
+    fi
+    sleep 60
+done
