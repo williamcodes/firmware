105cf976950464e15b05ffd288e76f2436348cdb
Harold Cooper <hrldcpr@gmail.com>
2015-09-03 23:15:45 -0400

    non-ascii character maybe messes with `supervisorctl log`

--- 3g.sh
+++ 3g.sh
@@ -1,9 +1,9 @@
 
 while true; do
-    echo 'unbinding USB…'
+    echo 'unbinding USB...'
     echo '1-1' > /sys/bus/usb/drivers/usb/unbind
     sleep 5
-    echo 'binding USB…'
+    echo 'binding USB...'
     echo '1-1' > /sys/bus/usb/drivers/usb/bind
     sleep 15
     # command copied from usb_modeswitch startup log in debug mode:
