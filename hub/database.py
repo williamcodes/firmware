88c83f78a587efd9d27109e8f6c3db6cf81f3283
Harold Cooper <hrldcpr@gmail.com>
2015-09-05 15:31:44 -0400

    don't iterate on cursors, use fetchall or fetchone

--- hub/database.py
+++ hub/database.py
@@ -17,39 +17,39 @@ class _Database:
         self.db = db
 
     def insert_temperature(self, cell_id, temperature, sleep_period):
-        with self.db as db:
-            db.execute('insert into temperatures (cell_id, temperature, sleep_period)'
-                       ' values (?, ?, ?)', (cell_id, temperature, sleep_period))
+        with self.db:
+            self.db.execute('insert into temperatures (cell_id, temperature, sleep_period)'
+                            ' values (?, ?, ?)', (cell_id, temperature, sleep_period))
 
     def get_unrelayed_temperatures(self):
-        with self.db as db:
-            return db.execute('select rowid, cell_id, temperature, sleep_period, time'
-                              ' from temperatures where relayed_time is null')
+        with self.db:
+            return self.db.execute('select rowid, cell_id, temperature, sleep_period, time'
+                                   ' from temperatures where relayed_time is null').fetchall()
 
     def set_relayed_temperature(self, id):
-        with self.db as db:
-            db.execute('update temperatures set relayed_time = strftime("%s", "now")'
-                       ' where rowid = ?', (id,))
+        with self.db:
+            self.db.execute('update temperatures set relayed_time = strftime("%s", "now")'
+                            ' where rowid = ?', (id,))
 
     def get_xbee_id(self):
-        with self.db as db:
-            (high, low), = db.execute('select xbee_id_high, xbee_id_low from status')
+        with self.db:
+            high, low = self.db.execute('select xbee_id_high, xbee_id_low from status').fetchone()
         if high and low:
             return high + low  # concatenate byte strings
 
     def set_xbee_id_high(self, high):
-        with self.db as db:
-            db.execute('update status set xbee_id_high = ?', (high,))
+        with self.db:
+            self.db.execute('update status set xbee_id_high = ?', (high,))
 
     def set_xbee_id_low(self, low):
-        with self.db as db:
-            db.execute('update status set xbee_id_low = ?', (low,))
+        with self.db:
+            self.db.execute('update status set xbee_id_low = ?', (low,))
 
     def get_sleep_period(self):
-        with self.db as db:
-            (sleep_period,), = db.execute('select sleep_period from status')
+        with self.db:
+            sleep_period, = self.db.execute('select sleep_period from status').fetchone()
         return sleep_period
 
     def set_sleep_period(self, sleep_period):
-        with self.db as db:
-            db.execute('update status set sleep_period = ?', (sleep_period,))
+        with self.db:
+            self.db.execute('update status set sleep_period = ?', (sleep_period,))
