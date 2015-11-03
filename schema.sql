c2943a0145ceff41e47791e2d3084273dad43095
William Jeffries <wm.jeffries@gmail.com>
2015-11-03 17:37:35 -0500

    revert

--- schema.sql
+++ schema.sql
@@ -4,3 +4,10 @@ create table temperatures (
        temperature not null,
        sleep_period not null,
        time not null default (strftime('%s', 'now')),
+       relayed_time
+);
+
+create table status as select
+       null as xbee_id_high,
+       null as xbee_id_low,
+       1 as sleep_period;
