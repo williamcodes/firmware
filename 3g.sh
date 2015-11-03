4d501587d30c05fa1a5e8e06d8683709bbe563ca
William Jeffries <wm.jeffries@gmail.com>
2015-11-03 17:37:09 -0500

    revert

--- 3g.sh
+++ 3g.sh
@@ -8,3 +8,10 @@ while true; do
     sleep 15
     # command copied from usb_modeswitch startup log in debug mode:
     usb_modeswitch --no-inquire --verbose --sysmode \
+                   --check-success 20 --configuration -1 \
+                   --default-vendor 12d1 --default-product 1446 \
+                   -c conf/usb_modeswitch.conf
+    sleep 10
+
+    wvdial
+done
